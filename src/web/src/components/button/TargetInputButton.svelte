9d6ea86ee46f94ab6a000ba6175b0620e4af3885
Ash M <ashleym9000@gmail.com>
2025-08-31 01:50:27 +0100

    Refactor AddHabitModal: extract TargetInputButton component, simplify target input handling, add description field, and improve layout styling.

---
+++ src/web/src/components/button/TargetInputButton.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import {CircleCheckBig} from '@lucide/svelte';
+
+    let {
+        target = (0),
+        unit = $bindable(),
+        frequency = $bindable()
+    } = $props();
+
+</script>
+<div class="dropdown flex-grow">
+    <div tabindex="0"
+         role="button"
+         class="align-middle btn btn-sm btn-outline font-normal border-base-content/20 hover:bg-primary-content/5">
+        <CircleCheckBig class="w-4 h-4"/>
+        <span class="text-sm">{target} {unit} per {frequency === 'daily' ? 'day' : 'week'}</span>
+    </div>
+    <div tabindex="0" class="dropdown-content shadow-2xl">
+        <div class="card bg-base-300 card-xs shadow-2xl">
+            <div class="card-body p-4 ">
+                <div class="flex gap-4">
+                    <div class="flex flex-col w-24">
+                        <label class="label">
+                            <span class="text-sm">Target</span>
+                        </label>
+                        <input type="number"
+                               bind:value={target}
+                               min="1"
+                               placeholder="15"
+                               class="input-simple"
+                        />
+                    </div>
+                    <div class="flex flex-col flex-1">
+                        <label class="label">
+                            <span class="text-sm">Unit</span>
+                        </label>
+                        <input type="text"
+                               bind:value={unit}
+                               placeholder="times"
+                               class="input-simple"
+                        />
+                    </div>
+                    <div class="flex flex-col flex-1">
+                        <label class="label">
+                            <span class="text-sm">Frequency</span>
+                        </label>
+                        <input type="text"
+                               bind:value={frequency}
+                               placeholder="daily"
+                               class="input-simple w-full"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/web/src/components/modals/AddHabitModal.svelte
+++ src/web/src/components/modals/AddHabitModal.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import Modal from './Modal.svelte'
     import type {Habit} from '$lib/habit/types';
-    import {CircleCheckBig} from '@lucide/svelte';
     import {validateHabit} from "$lib/habit/validation";
+    import TargetInputButton from "../button/TargetInputButton.svelte";
 
     let {
         open = $bindable(false),
@@ -50,60 +50,34 @@
     {#snippet content()}
         <div class="p-4">
             <div class="w-full ">
-                <input type="text" bind:value={name} placeholder="Name"
-                       class="input w-full text-xl font-semibold border-0 focus:outline-none focus:ring-0 bg-transparent"
+                <input type="text"
+                       bind:value={name}
+                       placeholder="Name"
+                       class="w-full text-xl font-semibold bg-transparent outline-none focus:ring-0 pl-2 pt-2 pb-2"
+                />
+                <input type="text"
+                       bind:value={name}
+                       placeholder="Description"
+                       class="w-full txt-sm font-semibold bg-transparent outline-none focus:ring-0 pl-2 pb-6"
                 />
             </div>
 
             <div class="pt-4 pl-2 w-full flex items-center gap-2">
-                <div class="dropdown flex-grow">
-                    <div tabindex="0" role="button" class="btn align-middle btn-sm btn-outline btn-neutral">
-                        <CircleCheckBig class="w-4 h-4"/>
-                        <span class="text-sm">{target} {unit} per {frequency === 'daily' ? 'day' : 'week'}</span>
-                    </div>
-                    <div tabindex="0" class="dropdown-content shadow-2xl">
-                        <div class="card bg-base-300 card-xs shadow-2xl">
-                            <div class="card-body p-4 ">
-                                <div class="flex gap-4">
-                                    <div class="flex flex-col w-24">
-                                        <label class="label">
-                                            <span class="text-sm">Target</span>
-                                        </label>
-                                        <input type="number"
-                                               bind:value={target}
-                                               min="1"
-                                               placeholder="15"
-                                               class="input-simple"
-                                        />
-                                    </div>
-                                    <div class="flex flex-col flex-1">
-                                        <label class="label">
-                                            <span class="text-sm">Unit</span>
-                                        </label>
-                                        <input type="text"
-                                               bind:value={unit}
-                                               placeholder="times"
-                                               class="input-simple"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--                <label class="floating-label flex-grow">-->
-                <!--                    <span>Unit</span>-->
-                <!--                    <input type="text" bind:value={unit} placeholder="reps" class="input-simple w-full"/>-->
-                <!--                </label>-->
-                <!--                <label class="floating-label flex-grow">-->
-                <!--                    <span>Frequency</span>-->
-                <!--                    <input type="text" bind:value={frequency} placeholder="daily" class="input-simple w-full"/>-->
-                <!--                </label>-->
-                <!--                <label class="floating-label flex-grow">-->
-                <!--                    <span>Frequency</span>-->
-                <!--                    <input type="text" placeholder="average" class="input-simple w-full"/>-->
-                <!--                </label>-->
+                <TargetInputButton bind:frequency={frequency} bind:target={target} bind:unit={unit}/>
             </div>
+            <!--                <label class="floating-label flex-grow">-->
+            <!--                    <span>Unit</span>-->
+            <!--                    <input type="text" bind:value={unit} placeholder="reps" class="input-simple w-full"/>-->
+            <!--                </label>-->
+            <!--                <label class="floating-label flex-grow">-->
+            <!--                    <span>Frequency</span>-->
+            <!--                    <input type="text" bind:value={frequency} placeholder="daily" class="input-simple w-full"/>-->
+            <!--                </label>-->
+            <!--                <label class="floating-label flex-grow">-->
+            <!--                    <span>Frequency</span>-->
+            <!--                    <input type="text" placeholder="average" class="input-simple w-full"/>-->
+            <!--                </label>-->
+
         </div>
     {/snippet}
 </Modal>
